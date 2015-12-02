3b7ad36d3e2109d03825ad86ecad1064cf0c9f12
wsong2 <weixus@hotmail.com>
2015-12-02 14:47:40 +0000

    Update psqlconv.sql

--- psqlconv.sql
+++ psqlconv.sql
@@ -3,3 +3,16 @@
 -- rawbuf position(5:25) raw
 -- update txttst set textn = utl_i18n.raw_to_nchar(rawbuf, 'utf8');
 -- update txttst set text1 = utl_i18n.raw_to_char(rawbuf);
+
+--dbms_pipe.pack_message	(p_message);
+--l_result := dbms_pipe.send_message ( l_pipe_id ); -- 'pipe_id'
+
+declare
+  l_message       varchar2(300);
+  l_result        pls_integer;
+begin
+  l_result := dbms_pipe.receive_message('pipd_id');
+  dbms_pipe.unpack_message(l_message);
+  dbms_output.put_line(l_message);
+end;
+/
