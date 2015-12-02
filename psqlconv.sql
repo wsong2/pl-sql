2e117d5a21b5c5bbf3dfea3e11322b10e4006745
wsong2 <weixus@hotmail.com>
2015-12-02 12:34:40 +0000

    Create psqlconv.sql

---
+++ psqlconv.sql
@@ -0,0 +1,5 @@
+-- l_raw := UTL_RAW.CONVERT(UTL_RAW.CAST_TO_RAW(p_string), 'American_America.AL32UTF8', 'American_America.AL16UTF16');
+
+-- rawbuf position(5:25) raw
+-- update txttst set textn = utl_i18n.raw_to_nchar(rawbuf, 'utf8');
+-- update txttst set text1 = utl_i18n.raw_to_char(rawbuf);
